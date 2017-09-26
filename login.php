a0ab3f43f4789129b3a8fce7c59d5024d0cd4480
John Cummings <jcummings5@gmail.com>
2017-09-26 11:05:27 -0400

    Added login

---
+++ login.php
@@ -0,0 +1,26 @@
+<?php
+require_once('rabbitMQLib.inc');
+
+if (!isset($_POST))
+{
+	$msg = "NO POST MESSAGE SET, POLITELY FUCK OFF";
+	echo json_encode($msg);
+	exit(0);
+}
+$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+$request = $_POST;
+$response = "unsupported request type, politely FUCK OFF";
+switch ($request["type"])
+{
+	case "login":
+		$req = array();
+		$req['type']="Login";
+		$req['username']=$request["uname"];
+		$req['password']=$request["password"];
+		$response = $client->send_request($req);
+	break;
+}
+echo json_encode($response);
+exit(0);
+
+?>
