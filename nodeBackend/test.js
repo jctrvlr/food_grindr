bddbddee53124735657c4a279b65559105bc2a25
jctrvlr <jcummings5@gmail.com>
2017-10-12 15:38:05 -0400

    Added test files

--- nodeBackend/test.js
+++ nodeBackend/test.js
@@ -22,8 +22,8 @@ amqp.connect('amqp://test:test@localhost:5672/testHost', function (err, conn) {
                     var loc = content.loc;
                     var lat = content.lat;
                     var lon = content.lon;
-                    console.log('Got message from get_loc queue: ', content);
-                    var r = {"message": "Response to get_loc message"};
+
+                    var r = getLocations(loc, lat, lon);
                     //var r = getLocations(loc, lat, lon);
                     ch.sendToQueue(msg.properties.replyTo, new Buffer(JSON.stringify(r)), { correlationId: msg.properties.correlationId });
                     ch.ack(msg);
@@ -33,3 +33,54 @@ amqp.connect('amqp://test:test@localhost:5672/testHost', function (err, conn) {
         });
     });
 });
+function getLocations(loc, lat, lon) {
+    let url = "https://developers.zomato.com/api/v2.1/locations?query=" + loc + "&lat=" + lat + "&lon=" + lon;
+    axios.get(url, {
+        headers: { 'Accept': 'application/json', 'user-key': key }
+    })
+        .then(function (response) {
+            console.log("Response from locations: ", response);
+            let ent_type = response.location_suggestions[0].entity_type;
+            let ent_id = response.location_suggestions[0].entity_id;
+            let zipcode = response.location_suggestions[0].zipcode;
+
+            let request = { "type": "insert_loc", "loc": loc, "ent_type": ent_type, "ent_id": ent_id, "lat": lat, "lon": lon };
+
+            amqp.connect('amqp://test:test@localhost:5672/testHost', function (err, conn) {
+                conn.createChannel(function (err, ch) {
+                    var q = 'loc';
+                    ch.assertQueue(q, { durable: false });
+                    ch.sendToQueue(msg.properties.replyTo, new Buffer(JSON.stringify(request)), { correlationId: msg.properties.correlationId });
+                });
+            });
+
+            let res = getRestaurants(ent_id, ent_type);
+            return res;
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+function getRestaurants(ent_id, ent_type) {
+    let url = "https://developers.zomato.com/api/v2.1/location_details?entity_id=" + ent_id + "&entity_type=" + ent_type;
+    axios.get(url, {
+        headers: { 'Accept': 'application/json', 'user-key': key }
+    })
+        .then(function (response) {
+            let rest_arr = response.best_rated_restaurant;
+            let request = { "type": "insert_res", "rest_arr": rest_arr };
+
+            amqp.connect('amqp://test:test@localhost:5672/testHost', function (err, conn) {
+                conn.createChannel(function (err, ch) {
+                    var q = 'loc';
+                    ch.assertQueue(q, { durable: false });
+                    ch.sendToQueue(msg.properties.replyTo, new Buffer(JSON.stringify(request)), { correlationId: msg.properties.correlationId });
+                });
+            });
+
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
--- test.data.php
+++ test.data.php
@@ -16,5 +16,65 @@ $request['lat'] = $lat;
 $request['lon'] = $lon;
 $response = $client->send_request($request);
 
+function insertRes1($res_arr) {
+  $output = insertRes($res_arr);
+  return $output;
+}
+
+function requestProcessor($request)
+{
+  echo "received request".PHP_EOL;
+  var_dump($request);
+  if(!isset($request['type']))
+  {
+    return "ERROR: unsupported message type";
+  }
+  switch ($request['type'])
+  {
+    case "get_rest":
+      return getRest($request['']); // figure out what requests
+    case "rest_response":
+      return restResp($request['email'], $request['rest_id'], $request['like'], $request['']);
+    case "insert_loc":
+      return insertLoc($request['loc'], $request['ent_type'], $request['ent_id'], $request['lat'], $request['lon']);
+    case "insert_res":
+      return insertRes1($request["rest_arr"]);
+  }
+  return array("returnCode" => '0', 'message'=>"Server received request and processed");
+}
+
+$server = new rabbitMQServer("rabbitMQData.ini","testServer");
+
+$server->process_requests('requestProcessor');
+exit();
+
+
+public function insertRes($res_arr) {
+    $json = json_decode($res_arr, true);
+        
+    foreach($json as $item) {
+        $id = $item[0]->id;
+        $name = $item[0]->name;
+        $location = $item[0]->location->address;
+        $zip = $item[0]->location->zipcode;
+        $lat = $item[0]->location->latitude;
+        $lon = $item[0]->location->longitude;
+        $cuisine = $item[0]->cuisine;
+        $online = $item[0]->has_online_delivery;
+        echo $id.PHP_EOL;
+        echo $name.PHP_EOL;
+        echo $location.PHP_EOL;
+        echo $zip.PHP_EOL;
+        echo $lat.PHP_EOL;
+        echo $lon.PHP_EOL;
+        echo $cuisine.PHP_EOL;
+        echo $online.PHP_EOL;
+
+        $statement = "insert ignore into restaurants (res_id, name, address, zipcode, longitude, latitude, cuisine, price_range, thumbnail, online_delivery) values ('";
+        echo $statement.PHP_EOL;
+    }
+    // iterate thru array and insert each restaurant
+
+}
 echo $response;
 ?>
