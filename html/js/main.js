d2cd55752ec1852ed605190a1ccb1a11eaaf5a13
katy-400 <katy.nyby@gmail.com>
2017-10-14 21:12:38 -0400

    added functions/vars to js and html file to output values

--- html/js/main.js
+++ html/js/main.js
@@ -1,6 +1,22 @@
-$(function(){
+/*$(function(){
   $('#toggle').click(function(){
     $('#target').toggleClass('active');
   });
 });
+*/
+console.log("Hi Katy, Im working");
+
+function poopValues(){
+var name = "Katy's Kitchen";
+var rating = 4.5;
+var address = "12 Bleeker Street Newark, NJ";
+var cuisine = "Irish Food";
+document.getElementById("name").innerHTML = "Paragraph changed!"; 
+document.getElementById("name").innerHTML  = name;
+document.getElementById("rating").innerHTML  = rating;
+document.getElementById("address").innerHTML  = address;
+document.getElementById("cuisine").innerHTML  = cuisine;
+alert("Hi Katy");
+}
+
 
