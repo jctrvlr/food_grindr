da77e5790fc98e488a9196ae53e14384a128f462
jctrvlr <jcummings5@gmail.com>
2017-10-16 19:01:21 -0400

    This?

--- nodeBackend/index.js
+++ nodeBackend/index.js
@@ -25,7 +25,7 @@ amqp.connect('amqp://test:test@localhost:5672/testHost', function (err, conn) {
                     var lon = content.lon;
                     var zip = content.zip;
                     console.log(loc, lat, lon, zip);
-                    var r = this.getLocations(loc, lat, lon);
+                    var r = getLocations(loc, lat, lon);
                     console.log(r);
                     ch.sendToQueue(msg.properties.replyTo, new Buffer(JSON.stringify(r)), { correlationId: msg.properties.correlationId });
                     ch.ack(msg);
