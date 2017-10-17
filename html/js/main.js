501c957c5bdd7c2237fc782363e82710d7912fb5
katy-400 <katy.nyby@gmail.com>
2017-10-16 22:30:39 -0400

    changed logo in HTMl; took out test alerts in JS

--- html/js/main.js
+++ html/js/main.js
@@ -4,3 +4,17 @@ $(function(){
   });
 });
 
+
+function poopValues(){
+var name = "Katy's Kitchen";
+var rating = 4.5;
+var address = "12 Bleeker Street Newark, NJ";
+var cuisine = "Irish Food";
+document.getElementById("name").innerHTML = "Paragraph changed!"; 
+document.getElementById("name").innerHTML  = name;
+document.getElementById("rating").innerHTML  = rating;
+document.getElementById("address").innerHTML  = address;
+document.getElementById("cuisine").innerHTML  = cuisine;
+}
+
+
