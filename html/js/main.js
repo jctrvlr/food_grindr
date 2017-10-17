107cccd9939349bf0e887e10e548c37f2cf9ca0c
katy-400 <katy.nyby@gmail.com>
2017-10-17 00:01:55 -0400

    changed alignment in css and added more variables in js

--- html/js/main.js
+++ html/js/main.js
@@ -6,15 +6,26 @@ $(function(){
 
 
 function poopValues(){
-var name = "Katy's Kitchen";
-var rating = 4.5;
-var address = "12 Bleeker Street Newark, NJ";
-var cuisine = "Irish Food";
+var name = "EXAMPLE";
+var rating = "Rating: ";
+var address = "Address: ";
+var phone = "Phone Number: "
+var price = "Price Range: "
+var cuisine = "Cuisine: ";
+var hours = "Hours: "
+var delivery = "Delivery: "
+/*Get a boolean to output yes or no*/
+
 document.getElementById("name").innerHTML = "Paragraph changed!"; 
 document.getElementById("name").innerHTML  = name;
 document.getElementById("rating").innerHTML  = rating;
 document.getElementById("address").innerHTML  = address;
+document.getElementById("phone").innerHTML = phone;
+document.getElementById("price").innerHTML = price;
 document.getElementById("cuisine").innerHTML  = cuisine;
+document.getElementById("hours").innerHTML = hours;
+document.getElementById("delivery").innerHTML  = delivery;
+
 }
 
 
