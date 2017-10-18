87da319b8b3680bfab43ecf376331abb00c190ad
KhariD <31661446+KhariD@users.noreply.github.com>
2017-10-18 16:45:48 -0400

    review.php

--- html/review.php
+++ html/review.php
@@ -4,33 +4,23 @@ require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 require_once('logger.inc');
 
-
 if (!isset($_POST))
 {
 	$msg = "NO POST MESSAGE SET, POLITELY FUCK OFF";
 	echo json_encode($msg);
 	exit(0);
 }
-$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+$client = new rabbitMQClient("rabbitMQReview.ini","testServer");
 $request = $_POST;
 
 switch ($request["type"])
 {
-	case "login": {
+	case "review": {
 		$req=array();
-		$req['type']="login";
+		$req['type']="review";
 		$req['email']=$request["email"];
-		$req['pass']=$request["pword"];
-		$response = $client->send_request($req);
-		break;
-	}
-	case "signup": {
-		$req = array();
-		$req['type'] = 'signup';
-		$req['email']=$request['email'];
-		$req['f_name']=$request['f_name'];
-		$req['l_name']=$request['l_name'];
-		$req['pass']=$request['pword'];
+		$req['resid']=$request["resid"];
+		$req['rating']=$request["rating"];
 		$response = $client->send_request($req);
 		break;
 	}
